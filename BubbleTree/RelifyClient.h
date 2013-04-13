183a4155a2f4c293c543933ab109a76d736151c0
bwestmoreland <git@westmoreland.us>
2013-04-13 17:41:07 -0400

    Update basic builder stub.

--- BubbleTree/RelifyClient.h
+++ BubbleTree/RelifyClient.h
@@ -6,6 +6,11 @@
 //  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
 //
 
+enum {
+    VenueBuilderInvalidJSONError,
+    VenueBuilderMissingDataError,
+};
+
 @interface RelifyClient : AFOAuth2Client
 
 +(RelifyClient *)sharedClient;
--- BubbleTree/RelifyClient.m
+++ BubbleTree/RelifyClient.m
@@ -23,4 +23,47 @@
     return _sharedClient;
 }
 
+- (NSArray *)venuesFromJSON:(NSString *)objectNotation
+                      error:(NSError *__autoreleasing *)error
+{
+    NSParameterAssert(objectNotation != nil);
+    
+    NSData *unicodeNotation = [objectNotation dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSError *localError = nil;
+    
+    id jsonObject = [NSJSONSerialization JSONObjectWithData:unicodeNotation
+                                                    options:0
+                                                      error:&localError];
+    NSDictionary *parsedObject = (id)jsonObject;
+    
+    if (parsedObject == nil){
+        if (error != NULL){
+            *error = [NSError errorWithDomain:VenueBuilderErrorDomain
+                                         code:VenueBuilderInvalidJSONError
+                                     userInfo:nil];
+        }
+        return nil;
+    }
+    
+    NSArray *venues = parsedObject[@"venues"];
+    
+    if (venues == nil){
+        if (error != NULL){
+            *error = [NSError errorWithDomain:VenueBuilderErrorDomain
+                                         code:VenueBuilderMissingDataError
+                                     userInfo:nil];
+        }
+        return nil;
+    }
+    NSMutableArray *results = [NSMutableArray arrayWithCapacity: [venues count]];
+    for (NSDictionary *parsedVenues in venues) {
+        //Do stuff with venues.
+    }
+    return [results copy];
+    
+}
+
+NSString *VenueBuilderErrorDomain = @"VenueBuilderErrorDomain";
+
 @end
