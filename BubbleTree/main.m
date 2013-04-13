cddd36cf0f14333631dd53177900bd853c940aae
bwestmoreland <git@westmoreland.us>
2013-04-13 15:37:05 -0400

    Initial Commit

---
+++ BubbleTree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BubbleTree
+//
+//  Created by Brent Westmoreland on 4/13/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BubbleTreeTests/BubbleTreeTests.m
@@ -0,0 +1,32 @@
+//
+//  BubbleTreeTests.m
+//  BubbleTreeTests
+//
+//  Created by Brent Westmoreland on 4/13/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+#import "BubbleTreeTests.h"
+
+@implementation BubbleTreeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BubbleTreeTests");
+}
+
+@end
