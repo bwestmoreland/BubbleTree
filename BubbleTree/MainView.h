dc552a68372ad7371843a59e13d210135aaa9ee1
bwestmoreland <git@westmoreland.us>
2013-04-13 16:57:00 -0400

    Change out the view color and create a MainView subclass

---
+++ BubbleTree/MainView.h
@@ -0,0 +1,11 @@
+//
+//  MainView.h
+//  BubbleTree
+//
+//  Created by Brent Westmoreland on 4/13/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+@interface MainView : UIView
+
+@end
