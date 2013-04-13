2055fe3aa87a2d5a3189f28f152502850075305e
bwestmoreland <git@westmoreland.us>
2013-04-13 16:23:35 -0400

    Add relify client stub

---
+++ BubbleTree/RelifyClient.m
@@ -0,0 +1,15 @@
+//
+//  RelifyClient.m
+//  BubbleTree
+//
+//  Created by Brent Westmoreland on 4/13/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+#import "RelifyClient.h"
+
+@implementation RelifyClient
+
+
+
+@end
