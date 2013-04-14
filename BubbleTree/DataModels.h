886837dad279a367514cfd7c011f6db975375456
bwestmoreland <git@westmoreland.us>
2013-04-13 23:40:24 -0400

    Update metadata.

--- BubbleTree/DataModels.h
+++ BubbleTree/DataModels.h
@@ -1,8 +1,8 @@
 //
 //  DataModels.h
 //
-//  Created by   on 4/13/13
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//  Created by Brent Westmoreland on 4/13/13
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
 //
 
 #import "Venue.h"
